a836a3cf98eadea786cb66472ef5658c07e0fda6
Monish-R45 <81694620+Monish-R45@users.noreply.github.com>
2021-06-15 16:36:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -117,7 +117,7 @@ function mouseReleased(){
 }
 function keyPressed(){
 	if(keyCode === 32){
-		Matter.Body.setPosition(stoneObj.Body, {x:235, y:420})
-		launcherObject.attach(stoneObj.Body)
+		Matter.Body.setPosition(stoneObj.body, {x:235, y:420})
+		launcherObject.attach(stoneObj.body)
 	}
 }
